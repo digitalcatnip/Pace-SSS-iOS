1ec72bcbade36e070f171d2fe3df3bcd5d3d2503
James McCarthy <james@catnip.io>
2016-08-15 11:03:10 -0400

    Initial background and root view controller

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Pace-SSS-iOS
-App for Pace University Special Support Services
+# Pace Special Support Services iOS App
+This is the codebase for the Pace University Special Support Services iOS app.
+Any significant notes that need to be recorded should go in this file for future reference.
+
+# Synx
+We use [Synx](https://github.com/venmo/synx) to keep the project folders
+organized.  Make sure you commit any changes before running synx to be able
+to revert any weird refactoring.
---
+++ SSS Freshman App/SSS Freshman App/RootGroup/RootVC.swift
@@ -0,0 +1,33 @@
+//
+//  RootVC.swift
+//  SSS Freshman App
+//
+//  Created by James McCarthy on 8/15/16.
+//  Copyright © 2016 Digital Catnip. All rights reserved.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+        super.viewWillDisappear(animated)
+    } 
+    
+}
